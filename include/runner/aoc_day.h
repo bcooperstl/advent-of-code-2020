1d9a6e295228d06803827086ae4a4d85f2a4dff2
bcooperstl <coop999@gmail.com>
2020-10-29 14:43:35 -0500

    Checking in AocDay base class for each day's implementations

---
+++ include/runner/aoc_day.h
@@ -0,0 +1,25 @@
+#ifndef __AOC_DAY_H__
+#define __AOC_DAY_H__
+
+#include <cstring>
+#include <vector>
+
+/* This is the superclass for all of the daily programs.
+ * It will have two functions to override - one for part 1, and one for part 2
+ * each of those functions takes two arguments
+ *  1) the filename for the input file.
+    2) a vector of strings for extra arguments. This vector can be empty.
+ */
+
+class AocDay
+{
+    protected:
+        int m_day;
+    public:
+        AocDay(int day);
+        ~AocDay();
+        virtual std::string part1(std::string filename, std::vector<std::string> extra_args);
+        virtual std::string part2(std::string filename, std::vector<std::string> extra_args);
+};
+
+#endif
---
+++ src/runner/aoc_day.cpp
@@ -0,0 +1,26 @@
+#include <cstring>
+#include <vector>
+#include <iostream>
+
+#include "aoc_day.h"
+
+AocDay::AocDay(int day):m_day(day)
+{
+}
+
+AocDay::~AocDay()
+{
+}
+
+std::string AocDay::part1(std::string filename, std::vector<std::string> extra_args)
+{
+    std::cerr << "*****Part 1 implementation for day " << m_day << " is not defined*****" << std::endl;
+    return "";
+}
+
+std::string AocDay::part2(std::string filename, std::vector<std::string> extra_args)
+{
+    std::cerr << "*****Part 2 implementation for day " << m_day << " is not defined*****" << std::endl;
+    return "";
+}
+
