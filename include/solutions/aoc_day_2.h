c37bc29e1985ecf04f5b29d2ac1e504faa5c6c7b
bcooperstl <coop999@gmail.com>
2020-12-02 14:50:38 -0600

    Added day 2 part 2 solution

--- include/solutions/aoc_day_2.h
+++ include/solutions/aoc_day_2.h
@@ -13,12 +13,14 @@ class PasswordFileLine
         static regex line_format;
         long m_min_occurrences;
         long m_max_occurrences;
+        long m_matching_positions[2];
         char m_letter;
         string m_password;
     public:
         PasswordFileLine(string input);
         ~PasswordFileLine();
-        bool isValid();
+        bool isValidPart1();
+        bool isValidPart2();
 };
 
 class AocDay2 : public AocDay
@@ -29,7 +31,7 @@ class AocDay2 : public AocDay
         AocDay2();
         ~AocDay2();
         string part1(string filename, vector<string> extra_args);
-        //string part2(string filename, vector<string> extra_args);
+        string part2(string filename, vector<string> extra_args);
 };
 
 
