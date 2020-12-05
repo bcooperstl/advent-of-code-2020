cd9e87cbd90d2240fc97f9789ce434dcdd0974d3
bcooperstl <coop999@gmail.com>
2020-12-05 13:56:19 -0600

    Day 4 part 2 solution; includes polymorphic AttributeValidator classess

--- include/solutions/aoc_day_4.h
+++ include/solutions/aoc_day_4.h
@@ -7,6 +7,65 @@
 
 using namespace std;
 
+#define BIRTH_YEAR "byr"
+#define ISSUE_YEAR "iyr"
+#define EXPIRATION_YEAR "eyr"
+#define HEIGHT "hgt"
+#define HAIR_COLOR "hcl"
+#define EYE_COLOR "ecl"
+#define PASSPORT_ID "pid"
+#define COUNTRY_ID "cid"
+
+class AttributeValidator
+{
+    protected:
+        string m_key;
+    public:
+        AttributeValidator(string key);
+        virtual bool isValidData(string input) = 0;
+        string getKey();
+};
+
+class YearValidator : public AttributeValidator
+{
+    private:
+        int m_min_year;
+        int m_max_year;
+    public:
+        YearValidator(string key, int min_year, int max_year);
+        bool isValidData(string input);
+};
+
+class HeightValidator : public AttributeValidator
+{
+    public:
+        HeightValidator();
+        bool isValidData(string input);
+};
+
+class HairColorValidator : public AttributeValidator
+{
+    public:
+        HairColorValidator();
+        bool isValidData(string input);
+};
+
+class EyeColorValidator : public AttributeValidator
+{
+private:
+        static vector<string> allowed_eye_colors;
+public:
+        EyeColorValidator();
+        bool isValidData(string input);
+};
+
+class PassportIdValidator : public AttributeValidator
+{
+    public:
+        PassportIdValidator();
+        bool isValidData(string input);
+};
+
 class PassportData
 {
     private:
@@ -16,19 +75,8 @@ class PassportData
         PassportData();
         ~PassportData();
         void setAttribute(string attribute, string value);
-        bool isValid();
-};
-
-class AttributeValidator
-{
-    public:
-        virtual bool isValidData(string input) = 0;
-};
-
-class BirthYearValidator : public AttributeValidator
-{
-    public:
-        bool isValidData(string input);
+        bool isValidAttributes();
+        bool isValidDataValues(vector<AttributeValidator *> validators);
 };
 
 class AocDay4 : public AocDay
@@ -39,7 +87,7 @@ class AocDay4 : public AocDay
         AocDay4();
         ~AocDay4();
         string part1(string filename, vector<string> extra_args);
-        //string part2(string filename, vector<string> extra_args);
+        string part2(string filename, vector<string> extra_args);
 };
 
 
