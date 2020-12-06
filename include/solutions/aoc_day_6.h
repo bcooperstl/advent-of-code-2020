6e2606d0fcc6d6dbd68dbfa49da0e951b12b0939
bcooperstl <coop999@gmail.com>
2020-12-06 14:12:40 -0600

    Added day 6 part 1 solution

---
+++ include/solutions/aoc_day_6.h
@@ -0,0 +1,47 @@
+#ifndef __AOC_DAY_6__
+#define __AOC_DAY_6__
+
+#include "aoc_day.h"
+
+#include <cstdint>
+
+using namespace std;
+
+class Person
+{
+    private:
+        string m_response;
+        uint32_t m_yeses;
+    public:
+        Person(string response);
+        ~Person();
+        string get_response();
+        uint32_t get_yeses();
+};
+
+class Group
+{
+    private:
+        vector<Person *> m_persons;
+        uint32_t m_group_yeses;
+    public:
+        Group();
+        ~Group();
+        void add_person(Person * person);
+        uint32_t get_group_yeses();
+        int get_yes_count();
+};
+
+class AocDay6 : public AocDay
+{
+    private:
+        vector<Group *> parse_input(string filename);
+    public:
+        AocDay6();
+        ~AocDay6();
+        string part1(string filename, vector<string> extra_args);
+        //string part2(string filename, vector<string> extra_args);
+};
+
+
+#endif
---
+++ src/solutions/aoc_day_6.cpp
@@ -0,0 +1,162 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <cstdint>
+#include <bitset>
+
+#include "aoc_day_6.h"
+#include "file_utils.h"
+
+using namespace std;
+
+#define YESES_INIT 0
+#define YES 0x01
+
+Person::Person(string response)
+{
+    m_response = response;
+    m_yeses = YESES_INIT;
+    
+    for (int i=0; i<response.length(); i++)
+    {
+        // a will correspond to the least significant bit. the left shift is 0
+        // z will correspond to the most significant bit. the left shift is 25
+        m_yeses = m_yeses | (YES << (response[i]-'a'));
+    }
+#ifdef DEBUG_BITOPS
+cout << "Person with responses of " << response << " results in " << std::bitset<32>(m_yeses) << endl;
+#endif
+}
+
+Person::~Person()
+{
+}
+
+string Person::get_response()
+{
+    return m_response;
+}
+
+uint32_t Person::get_yeses()
+{
+    return m_yeses;
+}
+
+Group::Group()
+{
+    m_group_yeses = YESES_INIT;
+}
+
+Group::~Group()
+{
+    for (int i=0; i<m_persons.size(); i++)
+    {
+        delete m_persons[i];
+    }
+    m_persons.clear();
+}
+
+void Group::add_person(Person * person)
+{
+#ifdef DEBUG_BITOPS
+    cout << "Pre: " << std::bitset<32>(m_group_yeses) << "  ";
+    cout << "Adding person with " << std::bitset<32>(person->get_yeses()) << "  ";
+#endif
+    m_persons.push_back(person);
+    m_group_yeses = m_group_yeses | person->get_yeses();
+#ifdef DEBUG_BITOPS
+    cout << "Post: " << std::bitset<32>(m_group_yeses) << endl;
+#endif
+}
+
+uint32_t Group::get_group_yeses()
+{
+    return m_group_yeses;
+}
+
+int Group::get_yes_count()
+{
+   int count = 0;
+   for (int i=0; i<26; i++)
+   {
+       if (m_group_yeses & (YES << i))
+       {
+           count++;
+       }
+   }
+#ifdef DEBUG_BITOPS
+   cout << "Group with: " << std::bitset<32>(m_group_yeses) << " has count " << count << endl;
+#endif
+return count;
+}
+
+
+AocDay6::AocDay6():AocDay(6)
+{
+}
+
+AocDay6::~AocDay6()
+{
+}
+
+/*
+* Create a new Group
+* Loop over the lines of input
+    * If a blank line is detected
+        * Append the current Group object to the list of groups
+        * Create a new Group object for the next entry
+    * Else (not a blank line)
+        * Create a new Person from the line
+        * Add that person to the current Group
+* Append the last Group object to the list.
+*/
+vector<Group *> AocDay6::parse_input(string filename)
+{
+    FileUtils fileutils;
+    vector<string> raw_lines;
+    vector<Group *> groups;
+    if (!fileutils.read_as_list_of_strings(filename, raw_lines))
+    {
+        cerr << "Error reading in the data from " << filename << endl;
+        return groups;
+    }
+    
+    Group * current = new Group();
+    for (vector<string>::iterator iter = raw_lines.begin(); iter != raw_lines.end(); ++iter)
+    {
+        string line = *iter;
+        if (line.length() == 0)
+        {
+            groups.push_back(current);
+            current = new Group();
+        }
+        else
+        {
+            Person * person = new Person(line);
+            current->add_person(person);
+        }
+    }
+    
+    groups.push_back(current);
+    
+    return groups;
+}
+
+string AocDay6::part1(string filename, vector<string> extra_args)
+{
+    vector<Group * > groups = parse_input(filename);
+    int total_yeses = 0;
+    
+    for (vector<Group *>::iterator group_iter = groups.begin(); group_iter != groups.end(); ++group_iter)
+    {
+        Group * group = *group_iter;
+        total_yeses += group->get_yes_count();
+        // Done with the group. Delete it here so I don't write another clean-up loop
+        delete group;
+    }
+    ostringstream out;
+    out << total_yeses;
+    return out.str();
+}
