4cbfb83280e6185cdd9fc6872a4107721b462001
bcooperstl <coop999@gmail.com>
2020-12-13 13:38:07 -0600

    Added day 10 part 1 solution

---
+++ include/solutions/aoc_day_10.h
@@ -0,0 +1,22 @@
+#ifndef __AOC_DAY_10__
+#define __AOC_DAY_10__
+
+#include <string>
+
+#include "aoc_day.h"
+
+using namespace std;
+
+class AocDay10 : public AocDay
+{
+    private:
+        vector<long> read_input(string filename);
+    public:
+        AocDay10();
+        ~AocDay10();
+        string part1(string filename, vector<string> extra_args);
+        //string part2(string filename, vector<string> extra_args);
+};
+
+
+#endif
