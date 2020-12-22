fa7331823863a5edae347c9e1cd0befcc6faaf77
bcooperstl <coop999@gmail.com>
2020-12-21 21:58:03 -0600

    Added day 13 part 2 program

--- include/solutions/aoc_day_13.h
+++ include/solutions/aoc_day_13.h
@@ -2,6 +2,7 @@
 #define __AOC_DAY_13__
 
 #include <string>
+#include <vector>
 
 #include "aoc_day.h"
 
@@ -11,11 +12,12 @@ class AocDay13 : public AocDay
 {
     private:
         void parse_input_part_1(string filename, long & timestamp, vector<long> & bus_ids );
+        void parse_input_part_2(string filename, vector<long> &bus_ids, vector<int> & positions);
     public:
         AocDay13();
         ~AocDay13();
         string part1(string filename, vector<string> extra_args);
-        //string part2(string filename, vector<string> extra_args);
+        string part2(string filename, vector<string> extra_args);
 };
 
 
