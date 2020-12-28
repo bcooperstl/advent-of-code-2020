85a8cfc2f3ac072103edf659e755610e9f1d929f
bcooperstl <coop999@gmail.com>
2020-12-28 14:24:05 -0600

    Added day 15 part 1 solution, part 2 as copy of part 1 solution. Part 2 runs, but I want to speed it up

---
+++ include/solutions/aoc_day_15.h
@@ -0,0 +1,18 @@
+#ifndef __AOC_DAY_15__
+#define __AOC_DAY_15__
+
+#include "aoc_day.h"
+
+class AocDay15 : public AocDay
+{
+    private:
+        vector<long> read_input(string filename);
+    public:
+        AocDay15();
+        ~AocDay15();
+        string part1(string filename, vector<string> extra_args);
+        string part2(string filename, vector<string> extra_args);
+};
+
+
+#endif
