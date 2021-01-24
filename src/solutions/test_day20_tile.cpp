d357c51ea9fa7950b7f95eec095f238e7031c2d7
bcooperstl <coop999@gmail.com>
2021-01-24 14:19:22 -0600

    Testing the border value setup

--- src/solutions/test_day20_tile.cpp
+++ src/solutions/test_day20_tile.cpp
@@ -1,10 +1,135 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
 #include "aoc_day_20.h"
 
 using namespace std;
 
+vector<string> get_test_case()
+{
+    vector<string> ret;
+    ret.push_back("........#.");
+    ret.push_back(".###......");
+    ret.push_back(".###......");
+    ret.push_back(".###......");
+    ret.push_back("#...##....");
+    ret.push_back("....##....");
+    ret.push_back("..........");
+    ret.push_back(".##......#");
+    ret.push_back(".##.......");
+    ret.push_back("...#......");
+    return ret;
+}
+
+Tile generate_base_tile()
+{
+    vector<string> test_data;
+    test_data.push_back("........#.");
+    test_data.push_back(".###......");
+    test_data.push_back(".###......");
+    test_data.push_back(".###......");
+    test_data.push_back("#...##....");
+    test_data.push_back("....##....");
+    test_data.push_back("..........");
+    test_data.push_back(".##......#");
+    test_data.push_back(".##.......");
+    test_data.push_back("...#......");
+    Tile tile(1111);
+    
+    tile.set_map(test_data);
+    return tile;
+}
+ 
+void compare_borders(map<Border, int> expected, map<Border, int> actual)
+{
+    if (expected[NorthFront] != actual[NorthFront])
+    {
+        cout << "***** NorthFront border mismatch!! Expected " << expected[NorthFront] << " Received " << actual[NorthFront] << "*****" << endl;
+    }
+    else
+    {
+        cout << "NorthFront border matches" << endl;
+    }
+    if (expected[EastFront] != actual[EastFront])
+    {
+        cout << "***** EastFront border mismatch!! Expected " << expected[EastFront] << " Received " << actual[EastFront] << "*****" << endl;
+    }
+    else
+    {
+        cout << "EastFront border matches" << endl;
+    }
+    if (expected[SouthFront] != actual[SouthFront])
+    {
+        cout << "***** SouthFront border mismatch!! Expected " << expected[SouthFront] << " Received " << actual[SouthFront] << "*****" << endl;
+    }
+    else
+    {
+        cout << "SouthFront border matches" << endl;
+    }
+    if (expected[WestFront] != actual[WestFront])
+    {
+        cout << "***** WestFront border mismatch!! Expected " << expected[WestFront] << " Received " << actual[WestFront] << "*****" << endl;
+    }
+    else
+    {
+        cout << "WestFront border matches" << endl;
+    }
+    if (expected[NorthBack] != actual[NorthBack])
+    {
+        cout << "***** NorthBack border mismatch!! Expected " << expected[NorthBack] << " Received " << actual[NorthBack] << "*****" << endl;
+    }
+    else
+    {
+        cout << "NorthBack border matches" << endl;
+    }
+    if (expected[EastBack] != actual[EastBack])
+    {
+        cout << "***** EastBack border mismatch!! Expected " << expected[EastBack] << " Received " << actual[EastBack] << "*****" << endl;
+    }
+    else
+    {
+        cout << "EastBack border matches" << endl;
+    }
+    if (expected[SouthBack] != actual[SouthBack])
+    {
+        cout << "***** SouthBack border mismatch!! Expected " << expected[SouthBack] << " Received " << actual[SouthBack] << "*****" << endl;
+    }
+    else
+    {
+        cout << "SouthBack border matches" << endl;
+    }
+    if (expected[WestBack] != actual[WestBack])
+    {
+        cout << "***** WestBack border mismatch!! Expected " << expected[WestBack] << " Received " << actual[WestBack] << "*****" << endl;
+    }
+    else
+    {
+        cout << "WestBack border matches" << endl;
+    }    
+}
+ 
+void test_base_tile()
+{
+    map<Border, int> expected_borders;
+    expected_borders[NorthFront] =2;
+    expected_borders[NorthBack]  =256;
+    expected_borders[EastFront]  =4;
+    expected_borders[EastBack]   =128;
+    expected_borders[SouthFront] =8;
+    expected_borders[SouthBack]  =64;
+    expected_borders[WestFront]  =16;
+    expected_borders[WestBack]   =32;
+    
+    Tile tile = generate_base_tile();
+    map<Border, int> actual_borders = tile.get_borders();
+    cout << "Testing after construction of tile" << endl;
+    compare_borders(expected_borders, actual_borders);
+}
+
 int main (int argc, char * argv[])
 {
-
+    test_base_tile();
 
     return 0;
 }
