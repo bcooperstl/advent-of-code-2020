8388ea4faa948d01a6bb4053c77e92a2ad3c317f
bcooperstl <coop999@gmail.com>
2021-02-16 14:56:21 -0600

    Added day 24 part 2

--- src/solutions/aoc_day_24.cpp
+++ src/solutions/aoc_day_24.cpp
@@ -14,6 +14,8 @@ using namespace std;
 #define WHITE_TILE '.'
 #define BLACK_TILE '@'
 #define PART1_BUFFER 5
+#define PART2_BUFFER 6
+#define PART2_BORDER 2
 
 AocDay24::AocDay24():AocDay(24)
 {
@@ -241,3 +243,238 @@ string AocDay24::part1(string filename, vector<string> extra_args)
     return out.str();
 }
 
+void AocDay24::determine_next_grid_size(Screen & hex_grid, int buffer, int & min_x, int & max_x, int & min_y, int & max_y)
+{
+    cout << "Determining next grid sizes; starting sizes  x:" << min_x << "-" << max_x << "  y:" << min_y << "-" << max_y << endl;
+
+    bool stopper = false;
+    int found_min_x, found_max_x, found_min_y, found_max_y;
+    
+    // looking for found_min_y
+    for (int y=min_y; y<=max_y; y++)
+    {
+        for (int x=min_x; x<=max_x; x++)
+        {
+            if (abs((y%2))==abs((x%2))) // only look on tiles, not void
+            {
+                if (hex_grid.get(x,y) == BLACK_TILE)
+                {
+                    found_min_y = y;
+                    stopper = true;
+                    break;
+                }
+            }
+        }
+        if (stopper == true)
+        {
+            break;
+        }
+    }
+    
+    stopper = false;
+    // looking for found_max_y
+    for (int y=max_y; y>=min_y; y--)
+    {
+        for (int x=min_x; x<=max_x; x++)
+        {
+            if (abs((y%2))==abs((x%2))) // only look on tiles, not void
+            {
+                if (hex_grid.get(x,y) == BLACK_TILE)
+                {
+                    found_max_y = y;
+                    stopper = true;
+                    break;
+                }
+            }
+        }
+        if (stopper == true)
+        {
+            break;
+        }
+    }
+    
+    stopper = false;
+    // looking for found_min_x
+    for (int x=min_x; x<=max_x; x++)
+    {
+        for (int y=min_y; y<=max_y; y++)
+        {
+            if (abs((y%2))==abs((x%2))) // only look on tiles, not void
+            {
+                if (hex_grid.get(x,y) == BLACK_TILE)
+                {
+                    found_min_x = x;
+                    stopper = true;
+                    break;
+                }
+            }
+        }
+        if (stopper == true)
+        {
+            break;
+        }
+    }
+    
+    stopper = false;
+    // looking for found_max_x
+    for (int x=max_x; x>=min_x; x--)
+    {
+        for (int y=min_y; y<=max_y; y++)
+        {
+            if (abs((y%2))==abs((x%2))) // only look on tiles, not void
+            {
+                if (hex_grid.get(x,y) == BLACK_TILE)
+                {
+                    found_max_x = x;
+                    stopper = true;
+                    break;
+                }
+            }
+        }
+        if (stopper == true)
+        {
+            break;
+        }
+    }
+    
+    cout << "After going through the grid, black cells are at  x:" << found_min_x << "-" << found_max_x << "  y:" << found_min_y << "-" << found_max_y << endl;
+    
+    if (abs(found_min_x) < found_max_x)
+    {
+        found_min_x = -1 * found_max_x;
+    }
+    else
+    {
+        found_max_x = -1 * found_min_x;
+    }
+    if (abs(found_min_y) < found_max_y)
+    {
+        found_min_y = -1 * found_max_y;
+    }
+    else
+    {
+        found_max_y = -1 * found_min_y;
+    }
+    cout << "After squaring off the region  x:" << found_min_x << "-" << found_max_x << "  y:" << found_max_y << "-" << found_max_y << endl;
+    
+    found_min_x-=buffer;
+    found_max_x+=buffer;
+    found_min_y-=buffer;
+    found_max_y+=buffer;
+    cout << "After applying buffer  x:" << found_min_x << "-" << found_max_x << "  y:" << found_max_y << "-" << found_max_y << endl;
+    
+    if (found_max_x > max_x)
+    {
+        min_x = found_min_x;
+        max_x = found_max_x;
+    }
+    if (found_max_y > max_y)
+    {
+        min_y = found_min_y;
+        max_y = found_max_y;
+    }
+    
+    cout << "After reconciling with original  x:" << min_x << "-" << max_x << "  y:" << min_y << "-" << max_y << endl;
+    return;
+}
+
+int AocDay24::get_num_black_neighbors(Screen & hex_grid, int x, int y)
+{
+    int counter = 0;
+    if (hex_grid.get(x+2, y) == BLACK_TILE) // east
+    {
+        counter++;
+    }
+    if (hex_grid.get(x+1, y+1) == BLACK_TILE) // southeast
+    {
+        counter++;
+    }
+    if (hex_grid.get(x-1, y+1) == BLACK_TILE) // southwest
+    {
+        counter++;
+    }
+    if (hex_grid.get(x-2, y) == BLACK_TILE) // west
+    {
+        counter++;
+    }
+    if (hex_grid.get(x-1, y-1) == BLACK_TILE) // northwest
+    {
+        counter++;
+    }
+    if (hex_grid.get(x+1, y-1) == BLACK_TILE) // northeast
+    {
+        counter++;
+    }
+    return counter;
+}
+
+vector<pair<int, int>> AocDay24::get_points_to_set_to_black(Screen & hex_grid, int min_x, int max_x, int min_y, int max_y)
+{
+    vector<pair<int, int>> points;
+    for (int y=min_y; y<=max_y; y++)
+    {
+        for (int x=min_x; x<=max_x; x++)
+        {
+            if (abs((y%2))==abs((x%2))) // shortcut - will set to white if both are odd or both are even. Will leave as blank otherwise
+            {
+                char color = hex_grid.get(x,y);
+                // white gets set to black if exactly 2 neighbors are black
+                if (color == WHITE_TILE)
+                {
+                    int num_black = get_num_black_neighbors(hex_grid, x, y);
+                    if (num_black == 2)
+                    {
+                        points.push_back(pair<int,int>(x,y));
+                    }
+                }
+                else if (color == BLACK_TILE) // flips to white if 0 or >2 black neighbors, so stays black on 1 or 2
+                {
+                    int num_black = get_num_black_neighbors(hex_grid, x, y);
+                    if (num_black == 1 || num_black == 2)
+                    {
+                        points.push_back(pair<int,int>(x,y));
+                    }
+                }
+            }
+        }
+    }
+    return points;
+}
+
+string AocDay24::part2(string filename, vector<string> extra_args)
+{
+    vector<string> lines = read_input(filename);
+    int min_x, max_x, min_y, max_y;
+    Screen * current = NULL;
+    
+    vector<Day24Tile> tiles = make_tiles(lines);
+    
+    determine_grid_sizes(tiles, PART2_BUFFER, min_x, max_x, min_y, max_y);
+    
+    current = new Screen(VOID, min_x, max_x, min_y, max_y);
+    init_white_tiles(*current, min_x, max_x, min_y, max_y);
+    
+    current->display();
+    for (vector<Day24Tile>::iterator pos = tiles.begin(); pos != tiles.end(); ++pos)
+    {
+        flip_tile(*current, *pos);
+    }
+    current->display();
+    
+    for (int i=1; i<=100; i++)
+    {
+        vector<pair<int, int>> next_round_black_points = get_points_to_set_to_black(*current, min_x + PART2_BORDER, max_x - PART2_BORDER, min_y + PART2_BORDER, max_y - PART2_BORDER);
+        determine_next_grid_size(*current, PART2_BUFFER, min_x, max_x, min_y, max_y);
+        delete current;
+        current = new Screen(VOID, min_x, max_x, min_y, max_y);
+        init_white_tiles(*current, min_x, max_x, min_y, max_y);
+        current->set_multi(next_round_black_points, BLACK_TILE);
+        cout << "Day " << i << ": there are " << current->num_matching(BLACK_TILE) << " black tiles" << endl;
+        current->display();
+    }
+    
+    ostringstream out;
+    out << current->num_matching(BLACK_TILE);
+    delete current;
+    return out.str();
+}
